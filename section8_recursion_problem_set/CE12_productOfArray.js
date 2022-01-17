15f9f1331f4b74ea672681de10f8ada57dc84cc7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-17 13:29:15 -0500

    product of array using recursion

---
+++ section8_recursion_problem_set/CE12_productOfArray.js
@@ -0,0 +1,10 @@
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(array) {
+  if (array.length === 0) return 1;
+
+  return array.pop() * productOfArray(array);
+}
+
+productOfArray([1, 2, 3, 10]); //?
