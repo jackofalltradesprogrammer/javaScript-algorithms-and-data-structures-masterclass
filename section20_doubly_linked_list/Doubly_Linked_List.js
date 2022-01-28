b4e237fdca8cc1580d6e0c68cbad646819667e96
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-28 04:25:00 -0500

    push() doubly linked list

--- section20_doubly_linked_list/Doubly_Linked_List.js
+++ section20_doubly_linked_list/Doubly_Linked_List.js
@@ -7,12 +7,36 @@ class Node {
 }
 
 class DoublyLinkedList {
-  constructor(val) {
+  constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  // push() adding a node to the end of the doubly linked list.
+  push(val) {
+    const newNode = new Node(val);
+    // no head & no tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    // has atleast one node
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-const node = new Node('mango');
-node; //?
+// const node = new Node('mango');
+// node;
+
+const list = new DoublyLinkedList();
+list.push('1');
+list.push('2');
+list.push('LAST ITEM!!');
+list;
