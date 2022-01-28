f4c227150e54bc1d69c4c0a7d0d6d5cb502b9227
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-28 04:08:02 -0500

    Node class & constructor for Doubly linked list

---
+++ section20_doubly_linked_list/Doubly_Linked_List.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(val, prev, next) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+const node = new Node('mango');
+node; //?
