3d61e30a7b0c0dd1d779104109f68d0762bfa5e7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-31 11:01:12 -0500

    Queue

---
+++ seciton21_stacks_queues/Queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor() {
+    this.val = null;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+}
