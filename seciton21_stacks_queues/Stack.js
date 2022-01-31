5c39cc282da4e5c10e2ee01425262d284f547d1c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-31 09:52:23 -0500

    pop() stack

--- seciton21_stacks_queues/Stack.js
+++ seciton21_stacks_queues/Stack.js
@@ -12,7 +12,7 @@ class Stack {
     this.length = length;
   }
 
-  // add value to the stack - the beginning of the list
+  // push(val) add value to the stack - the beginning of the list
   push(val) {
     const newNode = new Node(val);
     if (!this.first) {
@@ -20,15 +20,32 @@ class Stack {
       this.last = newNode;
     } else {
       const temp = this.first;
-      this.first = temp;
+      this.first = newNode;
       this.first.next = temp;
     }
     return this.length++; //?
+  }
+
+  // pop() - pop the last node from stack and decrement the length
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+    const removedNode = this.first;
+    this.first = removedNode.next;
+    removedNode.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.last = null;
+    }
+    return removedNode.val;
   }
 }
 
 const stack = new Stack();
 stack.push(1);
 stack.push(2);
+stack.pop(); //?
+stack.pop(); //?
 
 stack;
