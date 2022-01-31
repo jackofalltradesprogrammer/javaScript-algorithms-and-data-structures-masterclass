9128d3ea4fdc7291de269c5adf1505f959825cc3
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-01-31 11:56:45 -0500

    enqueue(val) Queue

--- seciton21_stacks_queues/Queue.js
+++ seciton21_stacks_queues/Queue.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor() {
-    this.val = null;
+  constructor(val) {
+    this.val = val;
     this.next = null;
   }
 }
@@ -11,4 +11,25 @@ class Queue {
     this.last = null;
     this.length = 0;
   }
+
+  // enqueue(val) add node to the queue
+  // adding at the end of the list
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return this.length++;
+  }
 }
+
+const queue = new Queue();
+queue.enqueue(5);
+queue.enqueue(7);
+queue.enqueue(8);
+queue;
