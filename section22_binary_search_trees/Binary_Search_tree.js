51b33dcbcc99aa37e2bbf80d19a329b9a0bbace1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-01 08:42:12 -0500

    insert(val) binary search tree

--- section22_binary_search_trees/Binary_Search_tree.js
+++ section22_binary_search_trees/Binary_Search_tree.js
@@ -10,12 +10,40 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // insert() - add a node to the binary search tree
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) return (this.root = newNode);
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          break;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          break;
+        }
+        current = current.right;
+      }
+    }
+    return this;
+  }
 }
 
 const tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(15);
+tree.insert(7);
+tree.insert(9);
 
 tree;
