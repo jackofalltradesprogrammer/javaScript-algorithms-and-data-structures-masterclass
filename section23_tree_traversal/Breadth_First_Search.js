2e8e1f07ea5c5bbfa9ea6009b993ef2549cb04b5
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-02 08:33:51 -0500

    breadth first search - tree

--- section23_tree_traversal/Breadth_First_Search.js
+++ section23_tree_traversal/Breadth_First_Search.js
@@ -4,12 +4,31 @@ class BreadthFirstSearch extends BinarySearchTree {
   constructor() {
     super();
   }
+
+  //BFS() - Implement Breadth first search
+  BFS() {
+    const queue = [this.root]; // using push() and shift() to add/remove nodes
+    const result = [];
+    let current = this.root;
+    // start by putting root's value
+    while (queue.length) {
+      const current = queue.shift();
+
+      result.push(current.value);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return result;
+  }
 }
 
 const tree = new BreadthFirstSearch();
 tree.insert(10);
+tree.insert(6);
 tree.insert(15);
-tree.insert(7);
-tree.insert(9);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS(); //?
 
 tree;
