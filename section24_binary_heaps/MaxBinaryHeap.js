e41fea5b3d3f474d5be01f539302c3cbe9da8cd7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-03 12:45:19 -0500

    extractMax() - Max Binary Heap

--- section24_binary_heaps/MaxBinaryHeap.js
+++ section24_binary_heaps/MaxBinaryHeap.js
@@ -23,15 +23,61 @@ class MaxBinaryHeap {
     this.values.push(value);
     this.bubbleUp();
   }
+
+  // bubbleDown(index)  - moves the value index down at the correct postioning
+  //                      for the max binary heap
+  bubbleDown(index = 0) {
+    const currentVal = this.values[index];
+    const length = this.values.length;
+    let childIndex = null;
+    while (index < length) {
+      let leftElement = null;
+      const leftIndex = 2 * index + 1;
+      let rightElement = null;
+      const rightIndex = 2 * index + 2;
+      if (leftIndex < length) {
+        leftElement = this.values[leftIndex];
+      }
+      if (rightIndex < length) {
+        rightElement = this.values[rightIndex];
+      }
+
+      if (currentVal < leftElement || currentVal < rightElement) {
+        if (leftElement > rightElement) {
+          childIndex = leftIndex;
+        } else {
+          childIndex = rightIndex;
+        }
+      } else {
+        break;
+      }
+      // Make a swap
+      this.values[index] = this.values[childIndex];
+      this.values[childIndex] = currentVal;
+      index = childIndex;
+    }
+  }
+  // extractMax() - remove the max element from the max binary heap
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return max;
+  }
 }
 
 const maxBinaryHeap = new MaxBinaryHeap();
-maxBinaryHeap.insert(10);
-maxBinaryHeap.insert(11);
-maxBinaryHeap.insert(9);
-maxBinaryHeap.insert(2);
-maxBinaryHeap.insert(20);
-maxBinaryHeap.insert(1);
-maxBinaryHeap.insert(199);
+maxBinaryHeap.insert(41);
+maxBinaryHeap.insert(39);
+maxBinaryHeap.insert(33);
+maxBinaryHeap.insert(18);
+maxBinaryHeap.insert(27);
+maxBinaryHeap.insert(12);
+maxBinaryHeap.insert(55);
+maxBinaryHeap.extractMax(); //?
+maxBinaryHeap.extractMax(); //?
 
 maxBinaryHeap;
