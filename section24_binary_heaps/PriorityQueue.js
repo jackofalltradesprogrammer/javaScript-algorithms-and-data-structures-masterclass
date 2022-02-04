042e72d0d1962df7c740d2de8008ce72cc7df3f1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-04 04:40:05 -0500

    PriorityQueue using MinBinaryHeap

---
+++ section24_binary_heaps/PriorityQueue.js
@@ -0,0 +1,16 @@
+// Implement Priority Queue using MinBinaryHeap
+class Node {
+  constructor() {
+    this.val = null;
+    this.priority = null;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+}
+
+const priorityQueue = new PriorityQueue();
+priorityQueue;
