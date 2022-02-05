eda643ba3811a59008b608dc91c5d115f13da3ca
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-05 04:18:57 -0500

    enqueue - Priority queue

--- section24_binary_heaps/PriorityQueue.js
+++ section24_binary_heaps/PriorityQueue.js
@@ -1,8 +1,8 @@
 // Implement Priority Queue using MinBinaryHeap
 class Node {
-  constructor() {
-    this.val = null;
-    this.priority = null;
+  constructor(value, priority) {
+    this.val = value;
+    this.priority = priority;
   }
 }
 
@@ -10,7 +10,33 @@ class PriorityQueue {
   constructor() {
     this.values = [];
   }
+
+  // bubbleUp - Rearranges the nodes in a min binary heap
+  bubbleUp(index = this.values.length - 1) {
+    const current = this.values[index];
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parentNode = this.values[parentIndex];
+      if (current.priority < parentNode.priority) {
+        this.values[index] = parentNode;
+        this.values[parentIndex] = current;
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // Enqueue(value, priority) - Add a node to the queue and rearrange the min binary heap
+  enqueue(value, priority) {
+    const node = new Node(value, priority);
+    this.values.push(node);
+    this.bubbleUp();
+  }
 }
 
 const priorityQueue = new PriorityQueue();
+priorityQueue.enqueue(20, 10);
+priorityQueue.enqueue(19, 11);
+priorityQueue.enqueue(30, 5);
 priorityQueue;
