5f44914a5f6f16f40b2b6ddddf7386d1e75aff0c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-09 04:31:33 -0500

    addVertex(vertex) - Graph

--- section26_graph/Graph.js
+++ section26_graph/Graph.js
@@ -2,4 +2,13 @@ class Graph {
   constructor() {
     this.adjacenyList = {};
   }
+
+  // addVertex(vertex) to the graph
+  addVertex(vertex) {
+    if (!this.adjacenyList[vertex]) this.adjacenyList[vertex] = [];
+  }
 }
+
+const graph = new Graph();
+graph.addVertex('Tokyo');
+graph;
