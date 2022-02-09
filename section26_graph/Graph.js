985a4bd766f8c4f753328c91084b3c41d90e6b4d
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-09 04:57:32 -0500

    removeEdge(vertex1, vertex2) - Graph

--- section26_graph/Graph.js
+++ section26_graph/Graph.js
@@ -13,6 +13,16 @@ class Graph {
     this.adjacenyList[vertex1].push(vertex2);
     this.adjacenyList[vertex2].push(vertex1);
   }
+
+  // removeEdge(vertex1, vertex2) remove corresponding edges for both the vertices
+  removeEdge(vertex1, vertex2) {
+    this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+      edge => edge !== vertex2,
+    );
+    this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter(
+      edge => edge !== vertex1,
+    );
+  }
 }
 
 const graph = new Graph();
@@ -21,4 +31,6 @@ graph.addVertex('Tokyo');
 graph.addVertex('Aspen');
 graph.addEdge('Dallas', 'Tokyo');
 graph.addEdge('Dallas', 'Aspen');
+graph.removeEdge('Dallas', 'Tokyo');
+graph.removeEdge('Dallas', 'Aspen');
 graph;
