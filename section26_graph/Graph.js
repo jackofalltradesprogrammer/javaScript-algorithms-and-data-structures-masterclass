87ccb14590855c3130868db233b58528565d64f9
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-02-09 04:41:22 -0500

    addEdge(vertex1, vertex2( - Graph

--- section26_graph/Graph.js
+++ section26_graph/Graph.js
@@ -7,8 +7,18 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacenyList[vertex]) this.adjacenyList[vertex] = [];
   }
+
+  // addEdge(vertex1, vertex2) add corresponding edges for both the vertices
+  addEdge(vertex1, vertex2) {
+    this.adjacenyList[vertex1].push(vertex2);
+    this.adjacenyList[vertex2].push(vertex1);
+  }
 }
 
 const graph = new Graph();
+graph.addVertex('Dallas');
 graph.addVertex('Tokyo');
+graph.addVertex('Aspen');
+graph.addEdge('Dallas', 'Tokyo');
+graph.addEdge('Dallas', 'Aspen');
 graph;
